cffd41b5e6ef8bb37d0ee75b0ea9659ed1f6fe81
Nikhil Varma <nikhilvarma892@gmail.com>
2020-11-18 20:37:43 +0530

    Feat: Add toolbox

---
+++ src/components/Toolbox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="toolbox">
+    <button class="tool" @click="togglePreview">
+      <i class="fa fa-columns"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toolbox',
+  props: {
+    togglePreviewMode: Function,
+  },
+  created: function() {
+    this.isPreviewEnabled = true;
+  },
+  methods: {
+    togglePreview: function() {
+      console.log('toggle');
+      this.togglePreviewMode();
+    },
+  },
+};
+</script>
+<style scoped>
+.toolbox {
+  width: 60px;
+  height: 100%;
+  background-color: rgb(34 58 82);
+  color: white;
+  text-align: center;
+  line-height: 60px;
+  border: none;
+  box-shadow: 0 -4px 22px -6px rgb(0 0 0 / 63%);
+}
+button {
+  background: transparent;
+  outline: none;
+  color: white;
+  font-size: 19px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  height: 70px;
+}
+button:hover {
+  background-color: #132536;
+}
+</style>
