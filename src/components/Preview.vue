99c2c787a8512fb1307b8bf2d613be21a9bfd45b
Nikhil Varma <nikhilvarma892@gmail.com>
2020-11-18 20:37:51 +0530

    Fix: Add Preview mode

---
+++ src/components/Preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="preview" v-html="renderedContent"></div>
+</template>
+
+<script>
+export default {
+  name: 'Preview',
+  props: {
+    renderedContent: HTMLAllCollection,
+  },
+};
+</script>
+<style scoped>
+.preview {
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  background-color: #1b2f42;
+  color: white;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
