6ca2c2a25aa8a2a27d1b23f55c39272e8f35a650
Nikhil Varma <nikhilvarma892@gmail.com>
2020-11-18 22:18:03 +0530

    Fix: Fix scroll glitch

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -14,13 +14,10 @@ export default {
 .preview {
   width: 100%;
   height: 100%;
-  overflow-y: hidden;
   background-color: #1b2f42;
   color: white;
+  overflow-y: scroll;
   padding: 20px;
   box-sizing: border-box;
-}
-.preview:hover {
-  overflow-y: scroll;
 }
 </style>
